d69e13eb87ab2013e3ec6d3fbe1dedf17a6c3eb6
Vinicius-de-Morais <vmorais194@gmail.com>
2023-04-20 16:39:43 -0300

    Terminando a logica de 2 jogadores e validação de vitoria

--- Desafio01/Desafio01/Program.cs
+++ Desafio01/Desafio01/Program.cs
@@ -7,13 +7,44 @@ public class JogoDaVelha
 {
     public static void Main(String[] args)
     {
+        playing();
+    }
+
+    public static void playing()
+    {
         string[,] jogoDaVelha = tabuleiro();
-        printTabuleiro(jogoDaVelha);
-        Console.WriteLine("Insira o valor da sua jogada");
-        string posicao = Console.ReadLine();
-        int[] play = jogada(jogoDaVelha, posicao);
-        Console.WriteLine(play);
-        printTabuleiro(jogoDaVelha);
+        bool stop = false;
+        string player = "X";
+
+
+        while (!stop)
+        {
+            printTabuleiro(jogoDaVelha);
+            Console.Write("Jogador: "+player+" insira a sua jogada: ");
+            string posicao = Console.ReadLine();
+
+
+
+            jogada(jogoDaVelha, posicao, player);
+            Console.Clear();
+            if(venceu(jogoDaVelha, player))
+            {
+                Console.WriteLine(@"
+ __     __                                                                                                  
+|  \   |  \                                                                                                 
+| $$   | $$  ______    _______   ______         __     __   ______   _______    _______   ______   __    __ 
+| $$   | $$ /      \  /       \ /      \       |  \   /  \ /      \ |       \  /       \ /      \ |  \  |  \
+ \$$\ /  $$|  $$$$$$\|  $$$$$$$|  $$$$$$\       \$$\ /  $$|  $$$$$$\| $$$$$$$\|  $$$$$$$|  $$$$$$\| $$  | $$
+  \$$\  $$ | $$  | $$| $$      | $$    $$        \$$\  $$ | $$    $$| $$  | $$| $$      | $$    $$| $$  | $$
+   \$$ $$  | $$__/ $$| $$_____ | $$$$$$$$         \$$ $$  | $$$$$$$$| $$  | $$| $$_____ | $$$$$$$$| $$__/ $$
+    \$$$    \$$    $$ \$$     \ \$$     \          \$$$    \$$     \| $$  | $$ \$$     \ \$$     \ \$$    $$
+     \$      \$$$$$$   \$$$$$$$  \$$$$$$$           \$      \$$$$$$$ \$$   \$$  \$$$$$$$  \$$$$$$$  \$$$$$$ 
+
+                                           Jogador "+player);
+                stop = true;
+            }
+            player = player == "X" ? "O" : "X";
+        }
     }
 
     public static string[,] tabuleiro()
@@ -26,11 +57,19 @@ public class JogoDaVelha
     }
     
     public static void printTabuleiro(string[,] tabuleiro)
-    {   
-        for(int i = 0; i < tabuleiro.GetLength(0); i++)
+    {
+        Console.WriteLine(
+              @" 
+                                     _   _      _             _             
+                                    | | (_)    | |           | |            
+                                    | |_ _  ___| |_ __ _  ___| |_ ___   ___ 
+                                    | __| |/ __| __/ _` |/ __| __/ _ \ / _ \
+                                    | |_| | (__| || (_| | (__| || (_) |  __/
+                                     \__|_|\___|\__\__,_|\___|\__\___/ \___|
+            ");
+        for (int i = 0; i < tabuleiro.GetLength(0); i++)
         {
-
-            Console.Write("  ");
+            Console.Write("\t\t\t\t\t\t ");
             for (int j = 0; j < tabuleiro.GetLength(1); j++)
             {
                 if(j+1 == tabuleiro.GetLength(1))
@@ -43,52 +82,78 @@ public class JogoDaVelha
             }
             if (i + 1 == tabuleiro.GetLength(1))
                 break;
-            Console.WriteLine(" ───────────");
+            Console.WriteLine("\t\t\t\t\t\t───────────");
         }
     }
 
-    public static int[] jogada(string[,] tabuleiro, string posicao)
+    public static void jogada(string[,] tabuleiro, string posicao, string player)
     {
-        int[] jogada = new int[2]; 
         for (int i = 0; i < tabuleiro.GetLength(0); i++)
         {
             for (int j = 0; j < tabuleiro.GetLength(1); j++)
             {
-                if (tabuleiro[i, j] == posicao)
+                if ((tabuleiro[i, j] != "X" && tabuleiro[i, j] != "O") && tabuleiro[i, j] == posicao)
                 {
-                    tabuleiro[i, j] = "X";
-                    jogada[0] = i;
-                    jogada[1] = j;
-
+                    tabuleiro[i, j] = player;
                 }
             }
         }
-        return jogada;
     }
 
-    public static bool venceu(string[,] tabuleiro)
+    public static bool venceu(string[,] tabuleiro, string player)
     {
-        string valorAnterior = "";
+
+        if(checkDiagonal(tabuleiro, player) || checkReverseDiagonal(tabuleiro, player) || checkLineAndColum(tabuleiro, player))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static bool checkDiagonal(string[,] tabuleiro, string player)
+    {
         for (int i = 0; i < tabuleiro.GetLength(0); i++)
         {
             for (int j = 0; j < tabuleiro.GetLength(1); j++)
             {
-                if (j > 1 && tabuleiro[i, j] == "X") ;
-                {
-
-                }
+                if (i==j && tabuleiro[i, j] != player)
+                    return false;
             }
-        }
 
-        for (int i = 0; i < valorAnterior.GetLength(0); i++)
+        }
+        return true;
+    }
+
+    public static bool checkReverseDiagonal(string[,] tabuleiro, string player)
+    {
+        int aux = tabuleiro.GetLength(1) -1;
+        for (int i = 0; i < tabuleiro.GetLength(0); i++)
         {
             while (aux >= 0)
             {
-                diagonalSec = valorAnterior[i, aux];
+                if (tabuleiro[i, aux] != player)
+                    return false;
+
                 aux--;
                 break;
             }
 
         }
+        return true;
+    }
+
+    public static bool checkLineAndColum(string[,] tabuleiro, string player)
+    {
+        for (int i = 0; i < tabuleiro.GetLength(0); i++)
+        {
+            for (int j = 0; j < tabuleiro.GetLength(1); j++)
+            {
+                if (tabuleiro[0, j] == player && tabuleiro[1, j] == player && tabuleiro[2, j] == player)
+                    return true;
+            }
+            if (tabuleiro[i, 0] == player && tabuleiro[i, 1] == player && tabuleiro[i, 2] == player)
+                return true;
+        }
+        return false;
     }
 }
